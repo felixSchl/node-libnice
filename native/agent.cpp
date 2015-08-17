1b082af395b09b74ed0cf24c70aa7fc99767c80f
Felix Schlitter <felixschlitter@gmail.com>
2015-08-18 10:05:44 +1200

    Expose all readable/writeable properties

--- native/agent.cpp
+++ native/agent.cpp
@@ -1,4 +1,5 @@
 #include <nan.h>
+#include <iostream>
 #include "Agent.h"
 
 namespace libnice {
@@ -36,15 +37,83 @@ namespace libnice {
     /**
      * Properties
      */
+
     Nan::SetAccessor(
       proto
     , Nan::New("controllingMode").ToLocalChecked()
     , Agent::GetControllingMode
     , Agent::SetControllingMode);
 
+    Nan::SetAccessor(
+      proto
+    , Nan::New("iceTcp").ToLocalChecked()
+    , Agent::GetIceTcp
+    , Agent::SetIceTcp);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("iceUdp").ToLocalChecked()
+    , Agent::GetIceUdp
+    , Agent::SetIceUdp);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("keepaliveConncheck").ToLocalChecked()
+    , Agent::GetKeepAliveConnCheck
+    , Agent::SetKeepAliveConnCheck);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("maxConnectivityChecks").ToLocalChecked()
+    , Agent::GetMaxConnectivityChecks
+    , Agent::SetMaxConnectivityChecks);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("proxyIp").ToLocalChecked()
+    , Agent::GetProxyIp
+    , Agent::SetProxyIp);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("proxyPassword").ToLocalChecked()
+    , Agent::GetProxyPassword
+    , Agent::SetProxyPassword);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("proxyPort").ToLocalChecked()
+    , Agent::GetProxyPort
+    , Agent::SetProxyPort);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("proxyType").ToLocalChecked()
+    , Agent::GetProxyType
+    , Agent::SetProxyType);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("proxyUsername").ToLocalChecked()
+    , Agent::GetProxyUsername
+    , Agent::SetProxyUsername);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("stunServer").ToLocalChecked()
+    , Agent::GetStunServer
+    , Agent::SetStunServer);
+
+    Nan::SetAccessor(
+      proto
+    , Nan::New("stunServerPort").ToLocalChecked()
+    , Agent::GetStunServerPort
+    , Agent::SetStunServerPort);
+
     /**
      * Export Agent
      */
+
     exports->Set(
       Nan::New("Agent").ToLocalChecked()
     , tpl->GetFunction());
@@ -52,6 +121,7 @@ namespace libnice {
     /**
      * Tie the knot
      */
+
     Agent::constructor.Reset(tpl->GetFunction());
   }
 
@@ -64,6 +134,14 @@ namespace libnice {
   void Agent::GatherCandidates(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     Agent* agent = ObjectWrap::Unwrap<Agent>(info.Holder());
   }
+
+  /**
+   * Properties
+   */
+
+  /**
+   * Get / Set controlling-mode
+   */
 
   NAN_GETTER(Agent::GetControllingMode) {
     bool value;
@@ -85,7 +163,341 @@ namespace libnice {
     g_object_set(
       G_OBJECT(agent->agent)
     , property_name, value->BooleanValue()
-    , NULL
-    );
+    , NULL);
   }
+
+  /**
+   * Get / Set ice-tcp
+   */
+
+  NAN_GETTER(Agent::GetIceTcp) {
+    bool value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "ice-tcp";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetIceTcp) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "ice-tcp";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->BooleanValue()
+    , NULL);
+  }
+
+  /**
+   * Get / Set ice-udp
+   */
+
+  NAN_GETTER(Agent::GetIceUdp) {
+    bool value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "ice-udp";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetIceUdp) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "ice-udp";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->BooleanValue()
+    , NULL);
+  }
+
+  /**
+   * Get / Set keepalive-conncheck
+   */
+
+  NAN_GETTER(Agent::GetKeepAliveConnCheck) {
+    bool value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "keepalive-conncheck";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetKeepAliveConnCheck) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "keepalive-conncheck";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->BooleanValue()
+    , NULL);
+  }
+
+  /**
+   * Get / Set max-connectivity-checks
+   */
+
+  NAN_GETTER(Agent::GetMaxConnectivityChecks) {
+    uint32_t value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "max-connectivity-checks";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetMaxConnectivityChecks) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "max-connectivity-checks";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->Uint32Value()
+    , NULL);
+  }
+
+  /**
+   * Get / Set proxy-ip
+   */
+
+  NAN_GETTER(Agent::GetProxyIp) {
+    gchar* value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-ip";
+
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    if (value != NULL) {
+      info.GetReturnValue().Set(
+        Nan::New(value)
+          .ToLocalChecked());
+      g_free(value);
+    }
+  }
+
+  NAN_SETTER(Agent::SetProxyIp) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-ip";
+    v8::String::Utf8Value param(value->ToString());
+    std::string from = std::string(*param);
+
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, from.c_str()
+    , NULL);
+  }
+
+  /**
+   * Get / Set proxy-password
+   */
+
+  NAN_GETTER(Agent::GetProxyPassword) {
+    gchar* value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-password";
+
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    if (value != NULL) {
+      info.GetReturnValue().Set(
+        Nan::New(value)
+          .ToLocalChecked());
+      g_free(value);
+    }
+  }
+
+  NAN_SETTER(Agent::SetProxyPassword) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-password";
+    v8::String::Utf8Value param(value->ToString());
+    std::string from = std::string(*param);
+
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, from.c_str()
+    , NULL);
+  }
+
+  /**
+   * Get / Set proxy-port
+   */
+
+  NAN_GETTER(Agent::GetProxyPort) {
+    uint32_t value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-port";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetProxyPort) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-port";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->Uint32Value()
+    , NULL);
+  }
+
+  /**
+   * Get / Set proxy-type
+   */
+
+  NAN_GETTER(Agent::GetProxyType) {
+    uint32_t value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-type";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetProxyType) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-type";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->Uint32Value()
+    , NULL);
+  }
+
+  /**
+   * Get / Set proxy-username
+   */
+
+  NAN_GETTER(Agent::GetProxyUsername) {
+    gchar* value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-username";
+
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    if (value != NULL) {
+      info.GetReturnValue().Set(
+        Nan::New(value)
+          .ToLocalChecked());
+      g_free(value);
+    }
+  }
+
+  NAN_SETTER(Agent::SetProxyUsername) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "proxy-username";
+    v8::String::Utf8Value param(value->ToString());
+    std::string from = std::string(*param);
+
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, from.c_str()
+    , NULL);
+  }
+
+  /**
+   * Get / Set stun-server
+   */
+
+  NAN_GETTER(Agent::GetStunServer) {
+    gchar* value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "stun-server";
+
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    if (value != NULL) {
+      info.GetReturnValue().Set(
+        Nan::New(value)
+          .ToLocalChecked());
+      g_free(value);
+    }
+  }
+
+  NAN_SETTER(Agent::SetStunServer) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "stun-server";
+    v8::String::Utf8Value param(value->ToString());
+    std::string from = std::string(*param);
+
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, from.c_str()
+    , NULL);
+  }
+
+  /**
+   * Get / Set stun-server-port
+   */
+
+  NAN_GETTER(Agent::GetStunServerPort) {
+    uint32_t value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "stun-server-port";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetStunServerPort) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "stun-server-port";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->Uint32Value()
+    , NULL);
+  }
+
+
 }
--- native/agent.h
+++ native/agent.h
@@ -22,6 +22,28 @@ namespace libnice {
 
       static NAN_GETTER(GetControllingMode);
       static NAN_SETTER(SetControllingMode);
+      static NAN_GETTER(GetIceTcp);
+      static NAN_SETTER(SetIceTcp);
+      static NAN_GETTER(GetIceUdp);
+      static NAN_SETTER(SetIceUdp);
+      static NAN_GETTER(GetKeepAliveConnCheck);
+      static NAN_SETTER(SetKeepAliveConnCheck);
+      static NAN_GETTER(GetMaxConnectivityChecks);
+      static NAN_SETTER(SetMaxConnectivityChecks);
+      static NAN_GETTER(GetProxyIp);
+      static NAN_SETTER(SetProxyIp);
+      static NAN_GETTER(GetProxyPassword);
+      static NAN_SETTER(SetProxyPassword);
+      static NAN_GETTER(GetProxyPort);
+      static NAN_SETTER(SetProxyPort);
+      static NAN_GETTER(GetProxyType);
+      static NAN_SETTER(SetProxyType);
+      static NAN_GETTER(GetProxyUsername);
+      static NAN_SETTER(SetProxyUsername);
+      static NAN_GETTER(GetStunServer);
+      static NAN_SETTER(SetStunServer);
+      static NAN_GETTER(GetStunServerPort);
+      static NAN_SETTER(SetStunServerPort);
 
       GMainLoop* main_loop;
       GMainContext* main_context;
