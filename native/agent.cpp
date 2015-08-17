2aacf22b17e0e7efc131eeede00337537c37557d
Felix Schlitter <felixschlitter@gmail.com>
2015-08-18 08:44:37 +1200

    Write getter/setter `controlling-mode` on agent

--- native/agent.cpp
+++ native/agent.cpp
@@ -3,6 +3,15 @@
 
 namespace libnice {
 
+  Agent::Agent() {
+    this->main_loop = g_main_loop_new(NULL, FALSE);
+    this->main_context = g_main_loop_get_context(main_loop);
+    this->agent = nice_agent_new(
+        this->main_context
+      , NICE_COMPATIBILITY_RFC5245
+    );
+  }
+
   Nan::Persistent<v8::Function> Agent::constructor;
 
   void Agent::Init(v8::Handle<v8::Object> exports) {
@@ -12,15 +21,26 @@ namespace libnice {
      * Prepare constructor template
      */
     auto tpl = Nan::New<v8::FunctionTemplate>(Agent::New);
-    tpl->SetClassName(Nan::New("Agent").ToLocalChecked());
     tpl->InstanceTemplate()->SetInternalFieldCount(1);
-
-    Agent::constructor.Reset(tpl->GetFunction());
+    tpl->SetClassName(Nan::New("Agent").ToLocalChecked());
+    auto proto = tpl->PrototypeTemplate();
 
     /**
      * Prototype
      */
-    Nan::SetPrototypeMethod(tpl, "gatherCandidates", Agent::GatherCandidates);
+    Nan::SetPrototypeMethod(
+      tpl
+    , "gatherCandidates"
+    , Agent::GatherCandidates);
+
+    /**
+     * Properties
+     */
+    Nan::SetAccessor(
+      proto
+    , Nan::New("controllingMode").ToLocalChecked()
+    , Agent::GetControllingMode
+    , Agent::SetControllingMode);
 
     /**
      * Export Agent
@@ -28,6 +48,11 @@ namespace libnice {
     exports->Set(
       Nan::New("Agent").ToLocalChecked()
     , tpl->GetFunction());
+
+    /**
+     * Tie the knot
+     */
+    Agent::constructor.Reset(tpl->GetFunction());
   }
 
   NAN_METHOD(Agent::New) {
@@ -38,5 +63,29 @@ namespace libnice {
 
   void Agent::GatherCandidates(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     Agent* agent = ObjectWrap::Unwrap<Agent>(info.Holder());
+  }
+
+  NAN_GETTER(Agent::GetControllingMode) {
+    bool value;
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "controlling-mode";
+    g_object_get(
+      G_OBJECT(agent->agent)
+    , property_name, &value
+    , NULL);
+
+    info.GetReturnValue().Set(Nan::New(value));
+  }
+
+  NAN_SETTER(Agent::SetControllingMode) {
+    Agent* agent = ObjectWrap::Unwrap<Agent>(info.This());
+
+    const gchar* property_name = "controlling-mode";
+    g_object_set(
+      G_OBJECT(agent->agent)
+    , property_name, value->BooleanValue()
+    , NULL
+    );
   }
 }
--- native/agent.h
+++ native/agent.h
@@ -3,17 +3,29 @@
 
 #include <v8.h>
 #include <nan.h>
+#include <glib.h>
+#include <nice/nice.h>
 
 namespace libnice {
-  class Agent : Nan::ObjectWrap {
+  class Agent : public Nan::ObjectWrap {
 
     public:
       static void Init(v8::Handle<v8::Object> exports);
 
     private:
+      explicit Agent();
+
       static Nan::Persistent<v8::Function> constructor;
+
       static NAN_METHOD(New);
       static NAN_METHOD(GatherCandidates);
+
+      static NAN_GETTER(GetControllingMode);
+      static NAN_SETTER(SetControllingMode);
+
+      GMainLoop* main_loop;
+      GMainContext* main_context;
+      NiceAgent* agent;
   };
 }
 
